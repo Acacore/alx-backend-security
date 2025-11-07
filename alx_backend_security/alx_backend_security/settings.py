f42bab1864bf115b840776f7b85ffd98580e7222
Acacore <mensart81@outllook.com>
2025-11-07 11:13:50 +0000

    credentials protected

--- alx_backend_security/alx_backend_security/settings.py
+++ alx_backend_security/alx_backend_security/settings.py
@@ -24,7 +24,7 @@ print("DJANGO_AUTORELOAD_MODE =", os.getenv("DJANGO_AUTORELOAD_MODE"))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uac3a&k_k=zl)#+7@-2n^#3p@7(4yam$4et@wzk*^t5v!g2%aw'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,10 +85,19 @@ WSGI_APPLICATION = 'alx_backend_security.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
